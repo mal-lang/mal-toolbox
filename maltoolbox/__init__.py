e533531616f39669af1f6571bb4b74bd5a18fb82
Andrei Buhaiu <buhaiu@kth.se>
2024-04-03 17:54:36 +0200

    Bump version to 0.0.33.

--- maltoolbox/__init__.py
+++ maltoolbox/__init__.py
@@ -1,5 +1,5 @@
 # -*- encoding: utf-8 -*-
-# MAL Toolbox v0.0.32
+# MAL Toolbox v0.0.33
 # Copyright 2024, Andrei Buhaiu.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -21,7 +21,7 @@ MAL-Toolbox Framework
 """
 
 __title__ = 'maltoolbox'
-__version__ = '0.0.32'
+__version__ = '0.0.33'
 __authors__ = ['Andrei Buhaiu',
     'Giuseppe Nebbione',
     'Nikolaos Kakouros',
