4b0809378150a08112eeba3062278b2578ec3886
Joakim Loxdal <loxdal@kth.se>
2024-05-06 13:15:14 +0200

    Add fixtures to conftest.py that can be used by unit tests

---
+++ tests/conftest.py
@@ -0,0 +1,44 @@
+"""Fixtures and helpers used in several test modules"""
+import os
+import pytest
+
+from maltoolbox.language import specification, LanguageClassesFactory
+from maltoolbox.model import Model
+
+def testdata_path(filename):
+    """Returns the absolute path of a test data file (in ./testdata)
+
+    Arguments:
+    filename    - filename to append to path of ./testdata
+    """
+    current_dir = os.path.dirname(os.path.realpath(__file__))
+    return os.path.join(current_dir, f"testdata/{filename}")
+
+
+@pytest.fixture
+def corelang_spec():
+    """Fixture that returns the coreLang language specification as dict"""
+    mar_file_path = testdata_path("org.mal-lang.coreLang-1.0.0.mar")
+    return specification.load_language_specification_from_mar(mar_file_path)
+
+
+@pytest.fixture
+def example_model(corelang_spec):
+    """Fixture that generates a model for tests
+
+    Uses coreLang specification (fixture) to create and return a
+    Model object defined in ./testdata/simple_example_model.json
+    """
+
+    # Init LanguageClassesFactor
+    lang_classes_factory = LanguageClassesFactory(corelang_spec)
+    lang_classes_factory.create_classes()
+
+    # Create instance model
+    instance_model = Model('Test model', corelang_spec, lang_classes_factory)
+
+    # Load model instance from json and return it
+    model_path = testdata_path("simple_example_model.json")
+    instance_model.load_from_file(model_path)
+
+    return instance_model
