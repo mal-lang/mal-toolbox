0ffa189e88e362cdd857338f752396351cd9ac6d
Joakim Loxdal <loxdal@kth.se>
2024-05-15 17:06:45 +0200

    Add placeholders for attackgraph analyzer tests

---
+++ tests/attackgraph/test_analyzer.py
@@ -0,0 +1,34 @@
+"""Tests for analyzers"""
+
+# Apriori analyzer
+# TODO: Add apriori analyzer test implementations
+from maltoolbox.attackgraph.analyzers import apriori
+
+def test_analyzers_apriori_propagate_viability_from_node():
+    """See if viability is propagated correctly"""
+    pass
+
+
+def test_analyzers_apriori_propagate_necessity_from_node():
+    """See if necessity is propagated correctly"""
+    pass
+
+
+def test_analyzers_apriori_evaluate_viability():
+    pass
+
+
+def test_analyzers_apriori_evaluate_necessity():
+    pass
+
+
+def test_analyzers_apriori_evaluate_viability_and_necessity():
+    pass
+
+
+def test_analyzers_apriori_calculate_viability_and_necessity():
+    pass
+
+
+def test_analyzers_apriori_prune_unviable_and_unnecessary_nodes():
+    pass
