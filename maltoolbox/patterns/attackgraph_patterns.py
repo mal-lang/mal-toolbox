97a084a4ea041fa72ecb1204174673c075a87943
Joakim Loxdal <loxdal@kth.se>
2024-05-17 10:19:56 +0200

    Use lambdas instead of just checking attributes

--- maltoolbox/patterns/attackgraph_patterns.py
+++ maltoolbox/patterns/attackgraph_patterns.py
@@ -1,115 +1,129 @@
-"""Functions for searching for patterns in the AttackGraph"""
+"""Utilities for finding patterns in the AttackGraph"""
 
 from __future__ import annotations
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from maltoolbox.attackgraph import AttackGraph, AttackGraphNode
+from typing import Callable
+class SearchPattern:
+    """A pattern consists of conditions, the conditions are used
+    to find matching paths in an attack graph"""
+    conditions: list[Condition]
 
+    def __init__(self, conditions):
+        self.conditions = conditions
+
+    def find_matches(self, graph: AttackGraph):
+        """Search through a graph for a pattern using
+        its conditions, and return matching paths of nodes
+
+        Args:
+        graph   - The graph to search in
+    
+        Return: list[list[AttackGraphNode]] of matching paths
+        """
+
+        # Find the starting nodes
+        condition = self.conditions[0]
+        starting_nodes = []
+        for node in graph.nodes:
+            if condition.matches(node):
+                starting_nodes.append(node)
+
+        matching_paths = []
+        for node in starting_nodes:
+            matching_paths.extend(
+                find_matches_recursively(node, self.conditions)
+            )
+
+        return matching_paths
 
 @dataclass
-class AttackGraphPattern:
-    """A pattern to search for in a graph"""
-    attributes: dict
+class SearchCondition:
+    """A filter has a condition that has to be true for a node to match"""
+
+    # `matches` should be a lambda that takes node as input and returns bool
+    # If lamdba returns True for a node, the node matches
+    # If the lamdba returns False for a node, the node does not match
+    matches: Callable[[AttackGraphNode], bool]
+
+    # It is possible to require/allow a Condition to repeat
     min_repeated: int = 1
     max_repeated: int = 1
 
-    def matches(self, node: AttackGraphNode):
-        """Returns true if pattern matches node"""
-        matches_pattern = True
-        for attr, value in self.attributes:
-            if getattr(node, attr) != value:
-                matches_pattern = False
-                break
-        return matches_pattern
-
     def can_match_again(self, num_matches):
-        """Returns true if pattern can be used again"""
+        """Returns true if condition can be used again"""
         return num_matches < self.max_repeated
 
     def must_match_again(self, num_matches):
-        """Returns true if pattern must match again to be fulfilled"""
+        """Returns true if condition must match again to be fulfilled"""
         return num_matches < self.min_repeated
-
-
-def find_in_graph(graph: AttackGraph, patterns: list[AttackGraphPattern]):
-    """Query a graph for a pattern of attributes"""
-
-    # Find the starting nodes
-    attribute = patterns[0].attributes[0]
-    starting_nodes = graph.get_nodes_by_attribute_value(
-         attribute[0], attribute[1]
-    )
-    matching_chains = []
-    for node in starting_nodes:
-        matching_chains += find_matches_recursively(node, patterns)
-
-    return matching_chains
 
 
 def find_matches_recursively(
         node: AttackGraphNode,
-        pattern_list: list[AttackGraphPattern],
-        current_chain=None,
-        matching_chains=None,
-        pattern_match_count=0
+        condition_list: list[Condition],
+        current_path=None,
+        matching_paths=None,
+        condition_match_count=0
     ):
-    """Follow a chain of attack graph nodes, check if they follow the pattern.
-    When a sequence of patterns is fulfilled for a sequence of nodes,
-    add the list of nodes to the returned `matching_chains`
+    """Find all paths of nodes that match the list of conditions.
+    When a sequence of conditions is fulfilled for a path of nodes,
+    add the path of nodes to the returned `matching_paths`
 
     Args:
-    node                - node to check if current `pattern` matches for
-    pattern_list        - will attempt to match first pattern against `node`
+    node                - node to check if current `condition` matches for
+    condition_list     - first condition in list will attempt match `node`
     matching_nodes      - list of matched nodes so far (builds up recursively)
-    pattern_match_count - the number of matches on current pattern so far
+    condition_match_count - the number of matches on current condition so far
 
-    Return: list of lists of AttackGraphNodes that match the pattern
+    Return: list of lists of AttackGraphNodes that match the condition
     """
-    # Init chain lists if None
-    current_chain = [] if current_chain is None else current_chain
-    matching_chains = [] if matching_chains is None else matching_chains
+    # Init path lists if None
+    current_path = [] if current_path is None else current_path
+    matching_paths = [] if matching_paths is None else matching_paths
 
-    current_pattern = pattern_list[0]
+    current_exp = condition_list[0]
 
-    if current_pattern.matches(node):
-        # Current node matches, add to current_chain and increment match_count
-        current_chain.append(node)
-        pattern_match_count += 1
+    if current_exp.matches(node):
+        # Current node matches, add to current_path and increment match_count
+        current_path.append(node)
+        condition_match_count += 1
 
-        if len(pattern_list) == 1 \
-            and not current_pattern.must_match_again(pattern_match_count):
-            # This is the last pattern in the chain,
-            # and the current chain is matching
-            matching_chains.append(current_chain)
+        if len(condition_list) == 1 \
+            and not current_exp.must_match_again(condition_match_count):
+            # This is the last condition in the path,
+            # and the current path is matching
+            matching_paths.append(current_path)
 
-        elif current_pattern.can_match_again(pattern_match_count):
-            # Pattern has matches left, run recursively with current pattern
+        elif current_exp.can_match_again(condition_match_count):
+            # Pattern has matches left, run recursively with current condition
             for child in node.children:
-                matching_chains = find_matches_recursively(
+                matching_paths = find_matches_recursively(
                     child,
-                    pattern_list,
-                    current_chain=current_chain,
-                    matching_chains=matching_chains,
-                    pattern_match_count=pattern_match_count
+                    condition_list,
+                    current_path=current_path,
+                    matching_paths=matching_paths,
+                    condition_match_count=condition_match_count
                 )
         else:
-            # Pattern has run out of matches, must move on to next pattern
+            # Pattern has run out of matches, must move on to next condition
             for child in node.children:
-                matching_chains = find_matches_recursively(
+                matching_paths = find_matches_recursively(
                     child,
-                    pattern_list[1:],
-                    current_chain=current_chain,
-                    matching_chains=matching_chains
+                    condition_list[1:],
+                    current_path=current_path,
+                    matching_paths=matching_paths
                 )
     else:
-        if not current_pattern.must_match_again(pattern_match_count)\
-            and len(pattern_list) > 1:
-            # Node did not match current pattern, but we can try with
-            # the next pattern since current one is 'fulfilled'
-            matching_chains = find_matches_recursively(
+        if not current_exp.must_match_again(condition_match_count)\
+            and len(condition_list) > 1:
+            # Node did not match current condition, but we can try with
+            # the next condition since current one is 'fulfilled'
+            matching_paths = find_matches_recursively(
                 node,
-                pattern_list[1:],
-                current_chain=current_chain,
-                matching_chains=matching_chains
+                condition_list[1:],
+                current_path=current_path,
+                matching_paths=matching_paths
             )
 
-    return matching_chains
+    return matching_paths
