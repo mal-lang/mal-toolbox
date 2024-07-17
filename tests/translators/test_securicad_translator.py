7f30760a683a31fb52c0b8a76eaac9846b670532
Andrei Buhaiu <buhaiu@kth.se>
2024-07-17 17:48:03 +0200

    Add securicad translator test

---
+++ tests/translators/test_securicad_translator.py
@@ -0,0 +1,34 @@
+"""Unit tests for AttackGraph functionality"""
+
+import pytest
+
+from conftest import path_testdata
+
+from maltoolbox.model import Model
+from maltoolbox.language import LanguageClassesFactory
+from maltoolbox.translators import securicad
+
+def test_securicad_translator(corelang_lang_graph):
+    """Test that we can load old securiCAD models """
+    scad_file = path_testdata("example_model.sCAD")
+    lang_classes_factory = LanguageClassesFactory(corelang_lang_graph)
+    scad_model = securicad.load_model_from_scad_archive(
+        scad_file,
+        corelang_lang_graph,
+        lang_classes_factory
+    )
+
+    assert len(scad_model.assets) > 0
+
+    scad_model_dict = scad_model._to_dict()
+
+    regular_model = Model.load_from_file(
+        path_testdata("scad_equivalent_model.yml"),
+        lang_classes_factory
+    )
+
+    assert len(regular_model.assets) > 0
+
+    regular_model_dict = regular_model._to_dict()
+
+    assert regular_model_dict == scad_model_dict
