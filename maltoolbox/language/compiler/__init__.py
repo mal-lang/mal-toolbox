6557ad3e5593bee7b7dcf5fa831580de0b50db59
Nikolaos Kakouros <nkakouros@google.com>
2024-11-05 16:52:12 +0000

    Don't assume includes are in the same dir as main.mal

--- maltoolbox/language/compiler/__init__.py
+++ maltoolbox/language/compiler/__init__.py
@@ -3,6 +3,7 @@
 
 import os
 import sys
+from pathlib import Path
 from typing import Optional
 
 from antlr4 import FileStream, CommonTokenStream
@@ -25,17 +26,22 @@ class MalCompiler:
         self.path = None
         self.current_file = None
 
-    def compile(self, malfile: Optional[str] = None):
-        if not self.path:
-            self.path = os.path.dirname(malfile)
+    def compile(self, malfile: Optional[Path|str] = None):
+        self.current_file = Path(malfile)
 
-        self.current_file = os.path.basename(malfile)
+        if not self.path:
+            # The compiler is starting now, keep track of the PWD.
+            self.path = self.current_file.parent
+        elif self.current_file.is_relative_to(self.path):
+            pass
+        elif self.current_file.is_absolute():
+            pass
+        else:
+            self.current_file = self.path / malfile
+
         patched_antrl_syntax_error.file = malfile
 
-        input_stream = FileStream(
-            os.path.join(self.path, self.current_file), encoding="utf-8"
-        )
-
+        input_stream = FileStream(self.current_file, encoding="utf-8")
         lexer = malLexer(input_stream)
         stream = CommonTokenStream(lexer)
         parser = malParser(stream)
