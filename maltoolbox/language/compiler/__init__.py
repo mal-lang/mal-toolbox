a884c7fb461c50ae0dc5fe0bc47129b01d5d73b7
Nikolaos Kakouros <nkak@kth.se>
2024-11-05 16:50:25 +0000

    Show file in parser error

--- maltoolbox/language/compiler/__init__.py
+++ maltoolbox/language/compiler/__init__.py
@@ -2,12 +2,22 @@
 # mypy: ignore-errors
 
 import os
+import sys
 from typing import Optional
 
 from antlr4 import FileStream, CommonTokenStream
+from antlr4.error.ErrorListener import ConsoleErrorListener
 from .mal_lexer import malLexer
 from .mal_parser import malParser
 from .mal_visitor import malVisitor
+
+
+def patched_antrl_syntax_error(self, recognizer, offendingSymbol, line, column, msg, e):
+    file = patched_antrl_syntax_error.file
+    print(f"{file}:{str(line)}:{str(column)}: {msg}", file=sys.stderr)
+
+
+ConsoleErrorListener.syntaxError = patched_antrl_syntax_error
 
 
 class MalCompiler:
@@ -20,10 +30,12 @@ class MalCompiler:
             self.path = os.path.dirname(malfile)
 
         self.current_file = os.path.basename(malfile)
+        patched_antrl_syntax_error.file = malfile
 
         input_stream = FileStream(
             os.path.join(self.path, self.current_file), encoding="utf-8"
         )
+
         lexer = malLexer(input_stream)
         stream = CommonTokenStream(lexer)
         parser = malParser(stream)
