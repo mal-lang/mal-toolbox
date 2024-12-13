221b3660c5eef34937ffb2bf5c39135e4d374f5f
Nikolaos Kakouros <nkakouros@google.com>
2024-12-13 13:22:14 +0000

    johan extra deepcopy fix

--- tests/language/test_languagegraph.py
+++ tests/language/test_languagegraph.py
@@ -28,3 +28,17 @@ def test_corelang_with_union_different_assets_same_super_asset():
 
     # Make sure that it can generate
     LanguageGraph.from_mar_archive(mar_file_path)
+
+
+def test_testlang_child_attack_step_extension():
+    mar_file_path = path_testdata("testLang-with-child-extension.mar")
+
+    # Make sure that it can generate
+    testlang_lang_graph_wo_extra_deepcopy = LanguageGraph.from_mar_archive(
+        mar_file_path, False)
+    testlang_lang_graph_with_extra_deepcopy = LanguageGraph.from_mar_archive(
+        mar_file_path, True)
+    assert testlang_lang_graph_wo_extra_deepcopy._to_dict() == \
+        testlang_lang_graph_with_extra_deepcopy._to_dict()
+    testlang_lang_graph_with_extra_deepcopy.save_to_file('tmp/testlang_wedc_lang_graph.yml')
+    testlang_lang_graph_wo_extra_deepcopy.save_to_file('tmp/testlang_woedc_lang_graph.yml')
