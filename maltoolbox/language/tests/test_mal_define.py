1ff521b9ac49f3bb685cb4c2ce9e15157fa54440
Tomás Fonseca <tfonseca@kth.se>
2025-02-21 14:36:38 +0000

    chore: Moved define tests to separe files.

--- maltoolbox/language/tests/test_mal_define.py
+++ maltoolbox/language/tests/test_mal_define.py
@@ -1,16 +1,22 @@
 from .mal_analyzer_test_wrapper import AnalyzerTestWrapper
 
+from pathlib import Path
+import pytest
+
 '''
 A file to test different cases of the `define` instruction in MAL.
 '''
+
+@pytest.mark.usefixtures("setup_test_environment")
+@pytest.mark.parametrize("setup_test_environment", [Path(__file__).parent / "fixtures/define_test_files"], indirect=True)
 
 def test_define_1() -> None:
     '''
     Defines only version.
     '''
-    AnalyzerTestWrapper('''
-    #version:"0.0.0"
-    ''').test(
+    AnalyzerTestWrapper(
+        test_file="test_define_1.mal"
+    ).test(
         error=True,
         defines=['version']
     )
@@ -19,9 +25,9 @@ def test_define_2() -> None:
     '''
     Defines only ID.
     '''
-    AnalyzerTestWrapper('''
-    #id: "org.mal-lang.testAnalyzer"
-    ''').test(
+    AnalyzerTestWrapper(
+        test_file="test_define_2.mal"
+    ).test(
         error=True,
         defines=['id']
     )
@@ -30,10 +36,9 @@ def test_define_3() -> None:
     '''
     Defines correct ID but wrong version.
     '''
-    AnalyzerTestWrapper('''
-    #id: "org.mal-lang.testAnalyzer"
-    #version:"version1"
-    ''').test(
+    AnalyzerTestWrapper(
+        test_file="test_define_3.mal"
+    ).test(
         error=True,
         defines=['id', 'version']
     )
@@ -42,10 +47,9 @@ def test_define_4() -> None:
     '''
     Defines correct version and ID.
     '''
-    AnalyzerTestWrapper('''
-    #id: "org.mal-lang.testAnalyzer"
-    #version:"0.0.0"
-    ''').test(
+    AnalyzerTestWrapper(
+        test_file="test_define_4.mal"
+    ).test(
         defines=['id', 'version']
     )
 
@@ -53,11 +57,9 @@ def test_define_5() -> None:
     '''
     Defines correct version and ID, but version twice.
     '''
-    AnalyzerTestWrapper('''
-    #id: "org.mal-lang.testAnalyzer"
-    #version:"0.0.0"
-    #version:"1.0.0"
-    ''').test(
+    AnalyzerTestWrapper(
+        test_file="test_define_5.mal"
+    ).test(
         defines=['id', 'version']
     )
 
@@ -66,11 +68,9 @@ def test_define_6() -> None:
     Defines correct version, ID.
     Defines Key with value.
     '''
-    AnalyzerTestWrapper('''
-    #id: "org.mal-lang.testAnalyzer"
-    #version:"0.0.0"
-    #key:"value"
-    ''').test(
+    AnalyzerTestWrapper(
+        test_file="test_define_6.mal"
+    ).test(
         defines=['id', 'version', 'key']
     )
 
@@ -79,12 +79,9 @@ def test_define_7() -> None:
     Defines correct version, ID.
     Defines Key with value twice.
     '''
-    AnalyzerTestWrapper('''
-    #id: "org.mal-lang.testAnalyzer"
-    #version:"0.0.0"
-    #key:"value"
-    #key:"value"
-    ''').test(
+    AnalyzerTestWrapper(
+        test_file="test_define_7.mal"
+    ).test(
         error=True,
         defines=['id', 'version', 'key']
     )
@@ -93,10 +90,8 @@ def test_define_8() -> None:
     '''
     Defines correct version and ID, but id twice.
     '''
-    AnalyzerTestWrapper('''
-    #id: "org.mal-lang.testAnalyzer"
-    #version:"0.0.0"
-    #version:"1.0.0"
-    ''').test(
+    AnalyzerTestWrapper(
+        test_file="test_define_8.mal"
+    ).test(
         defines=['id', 'version']
     )
