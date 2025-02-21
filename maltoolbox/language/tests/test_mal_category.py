af410a294fb7f19baff764b18ca82dd3386f7794
Tomás Fonseca <tfonseca@kth.se>
2025-02-21 16:53:08 +0000

    chore: Moved category test snippets to separate files.
    Also deleted an analyzer check which is already done by the lexer.

--- maltoolbox/language/tests/test_mal_category.py
+++ maltoolbox/language/tests/test_mal_category.py
@@ -1,54 +1,35 @@
 from .mal_analyzer_test_wrapper import AnalyzerTestWrapper
 
+from pathlib import Path
+import pytest
+
 '''
 A file to test different cases of the `category` instruction in MAL.
 '''
 
+@pytest.mark.usefixtures("setup_test_environment")
+@pytest.mark.parametrize("setup_test_environment", [Path(__file__).parent / "fixtures/category_test_files"], indirect=True)
+
 def test_category_1() -> None:
     '''
     Defines correct version and ID.
-    Defines category without name.
+    Defines category with name.
     '''
-    AnalyzerTestWrapper('''
-    #id: "org.mal-lang.testAnalyzer"
-    #version:"0.0.0"
-
-    category {}
-                                        
-    ''').test(
-        error=True,
-        defines=['id', 'version']
+    AnalyzerTestWrapper(
+        test_file="test_category_1.mal"
+    ).test(
+        defines=['id', 'version'],
+        categories=['Test']
     )
 
 def test_category_2() -> None:
     '''
     Defines correct version and ID.
-    Defines category with name.
-    '''
-    AnalyzerTestWrapper('''
-    #id: "org.mal-lang.testAnalyzer"
-    #version:"0.0.0"
-
-    category Test {}
-                                        
-    ''').test(
-        defines=['id', 'version'],
-        categories=['Test']
-    )
-
-def test_category_3() -> None:
-    '''
-    Defines correct version and ID.
     Defines category with same name twice.
     '''
-    AnalyzerTestWrapper('''
-    #id: "org.mal-lang.testAnalyzer"
-    #version:"0.0.0"
-
-    category Test {}
-    category Test {}
-                                        
-    ''').test(
+    AnalyzerTestWrapper(
+        test_file="test_category_2.mal"
+    ).test(
         defines=['id', 'version'],
         categories=['Test']
     )
