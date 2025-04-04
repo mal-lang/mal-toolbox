e847a51003317de044104f45c9e8dd19d19e2238
Andrei Buhaiu <buhaiu@kth.se>
2025-04-04 17:13:06 +0200

    Rework how ttc calculations work

---
+++ tests/test_probs_utils.py
@@ -0,0 +1,31 @@
+"""Unit tests for the probabilities utilities module"""
+
+import pytest
+
+from maltoolbox.model import Model
+from maltoolbox.attackgraph.attackgraph import AttackGraph
+from maltoolbox.probs_utils import calculate_ttc, TTCCalculation
+
+def test_probs_utils(model: Model):
+    """Test TTC calculation for nodes"""
+
+    app = model.add_asset('Application')
+    creds = model.add_asset('Credentials')
+    user = model.add_asset('User')
+    identity = model.add_asset('Identity')
+    vuln = model.add_asset('SoftwareVulnerability')
+
+    identity.add_associated_assets('credentials', {creds})
+    user.add_associated_assets('userIds', {identity})
+    app.add_associated_assets('highPrivAppIAMs', {identity})
+    app.add_associated_assets('vulnerabilities', {vuln})
+
+    attack_graph = AttackGraph(model.lang_graph, model)
+
+    for node in attack_graph.nodes.values():
+        #TODO: Actually check some of the results
+        calculate_ttc(node.ttc, TTCCalculation.SAMPLE)
+
+    for node in attack_graph.nodes.values():
+        #TODO: Actually check some of the results
+        calculate_ttc(node.ttc, TTCCalculation.EXPECTED)
