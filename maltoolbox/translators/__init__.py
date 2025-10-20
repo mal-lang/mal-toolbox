ebc8f34f826997eff84d9def00805d98bec87aed
sandorstormen <sandor.berglund@gmail.com>
2025-10-20 16:54:12 +0200

    add: gexf format conversion for attack graphs

--- maltoolbox/translators/__init__.py
+++ maltoolbox/translators/__init__.py
@@ -1,8 +1,10 @@
 from .networkx import attack_graph_to_nx, model_to_nx
 from .updater import load_model_from_older_version
+from .gexf import attack_graph_to_gexf
 
 __all__ = [
     'attack_graph_to_nx',
     'model_to_nx',
-    'load_model_from_older_version'
+    'load_model_from_older_version',
+    'attack_graph_to_gexf'
 ]
