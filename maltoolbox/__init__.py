4cc017efb9c4e37287ca518574bc4f08f87cbabe
sandorstormen <sandor.berglund@gmail.com>
2026-08-31 08:45:59 +0200

    bump version to 2.10.4 in __init__.py and pyproject.toml

--- maltoolbox/__init__.py
+++ maltoolbox/__init__.py
@@ -1,4 +1,4 @@
-# MAL Toolbox v2.10.3
+# MAL Toolbox v2.10.4
 # Copyright 2025, Andrei Buhaiu.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -18,7 +18,7 @@
 """MAL-Toolbox Framework"""
 
 __title__ = 'maltoolbox'
-__version__ = '2.10.3'
+__version__ = '2.10.4'
 __authors__ = [
     'Andrei Buhaiu',
     'Giuseppe Nebbione',
