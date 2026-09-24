e37b14a1cd94f8a74f19357921f7296915742adb
sandorstormen <sandor.berglund@gmail.com>
2026-09-24 15:41:08 +0200

    refactor: reorganize GEXF imports and update __all__ exports

--- maltoolbox/translators/__init__.py
+++ maltoolbox/translators/__init__.py
@@ -1,12 +1,14 @@
-from .gexf import attack_graph_to_gexf, model_to_gexf, save_gexf_to_file
 from .networkx import attack_graph_to_nx, model_to_nx
 from .updater import load_model_from_older_version
 
 __all__ = [
-    'attack_graph_to_gexf',
     'attack_graph_to_nx',
     'load_model_from_older_version',
-    'model_to_gexf',
     'model_to_nx',
-    'save_gexf_to_file',
 ]
+
+try:
+    from .gexf import attack_graph_to_gexf, model_to_gexf, save_gexf_to_file
+    __all__ += ['attack_graph_to_gexf', 'model_to_gexf', 'save_gexf_to_file']
+except ImportError:
+    pass
